754f54f216506c6721aa614ab5c613302bd9dc93
Amulyasubhas <112745448+Amulyasubhas@users.noreply.github.com>
2023-04-11 08:57:18 +0530

    Add files via upload

---
+++ regular_assignment/week_6/Array (1).java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+  class Array{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int n,pos;
+      int arr[]=new int[n+1];
+      for(int i=0;i<n+1;i++){
+        a[i]=sc.nextInt();
+        
+      }
+      System.out.println("enter the position where you want to insert");
+      pos=sc.nextInt();
+      System.out.println("enter the element you want to insert");
+      for(int i=0;i<n+1;i++){
+        System.out.println(a[i]);
+      }
+    }
+  }
