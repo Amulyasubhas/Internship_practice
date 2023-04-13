8e11f3006c449697698870cbc17926b51c95fb32
Amulyasubhas <112745448+Amulyasubhas@users.noreply.github.com>
2023-04-13 12:40:25 +0530

    Add files via upload

---
+++ macro_assignment/week_3/NumberGuessGame.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.random;
+  class NumberGuessGame{
+    public static void main(String args[]){
+      scanner sc=new Scanner(System.in);
+      Random random=new Random();
+      randomnumber=random.nextInt(100);
+      
+    }
+  }
